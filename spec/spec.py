89ddb3d406c8ad2e99c6ad768b8cc892f5a9a9bb
Daniel Kudrow <dkudrow@cs.ucsb.edu>
2015-02-27 08:46:26 -0800

    add hooks, deps to yaml; validate fields

--- spec/spec.py
+++ spec/spec.py
@@ -1,5 +1,20 @@
 import yaml
 import sys
+from collections import namedtuple
+
+
+class NodeType(namedtuple('NodeType',
+                          'min_nodes max_nodes start_hook stop_hook deps')):
+
+    def _println(self, indent, field):
+        print '%s%s: %s' % (indent*'\t', field, str(getattr(self, field)))
+
+    def dump(self, indent=0):
+        self._println(indent, 'min_nodes')
+        self._println(indent, 'max_nodes')
+        self._println(indent, 'start_hook')
+        self._println(indent, 'stop_hook')
+        self._println(indent, 'deps')
 
 
 class Spec(object):
@@ -12,8 +27,13 @@ class Spec(object):
 
     --- # spec.yaml
     node_name:
-        min: <int>      # minimum nodes necessary for functional deployment
-        max: <int>      # nodes past which no benefit is derived
+        min_nodes:  <int>     # min. nodes needed for functional deployment
+        max_nodes:  <int>     # max num. useful nodes (null indicates no max.)
+        start_hook: <string>  # command to start node (abs. path)
+        stop_hook:  <string>  # command to stop node (abs. path)
+        deps:                 # list of dependencies on other node types
+            - <string>: <int> # name of node type that this type depends on
+                              # max. ratio of this type to dependency type
     ...
 
     The current state of the deployment can be polled from the ZooKeeper
@@ -22,18 +42,35 @@ class Spec(object):
     """
 
     def __init__(self, filename):
-        self._spec = {}
+        self._node_types = {}
+
+        # open and parse specification
         try:
             spec_file = open(filename, 'r')
-            self._spec = yaml.load(spec_file)
+            spec = yaml.load(spec_file)
         except IOError:
             raise ValueError('Could not open spec file: \'%s\'' % filename)
         except yaml.YAMLError:
             raise ValueError('Error reading spec file: \'%s\'' % filename)
 
+        for type_name in iter(spec):
+            try:
+                node_type = NodeType(min_nodes=spec[type_name]['min_nodes'],
+                                     max_nodes=spec[type_name]['max_nodes'],
+                                     start_hook=spec[type_name]['start_hook'],
+                                     stop_hook=spec[type_name]['stop_hook'],
+                                     deps=spec[type_name]['deps'])
+                self._node_types[type_name] = node_type
+            except KeyError, key:
+                raise ValueError('Node type \'%s\' missing required field %s' %
+                                 (type_name, key))
+
+        print self._node_types
+
     def dump(self):
-        for node in iter(self._spec):
-            print node, '->', self._spec[node]
+        for type_name in iter(self._node_types):
+            print '%s -> ' % type_name
+            self._node_types[type_name].dump(1)
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
