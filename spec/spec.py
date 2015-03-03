412c0c22d6e18fd993f7e965732f40d4b1cd8ea9
Daniel Kudrow <dkudrow@cs.ucsb.edu>
2015-03-03 10:00:42 -0800

    infer min_nodes from deps

--- spec/spec.py
+++ spec/spec.py
@@ -4,20 +4,8 @@ from math import ceil
 from collections import namedtuple
 
 
-class Role(object):
-
-    def __init__(self, min_nodes, max_nodes, start_hook, stop_hook, deps):
-        self.min_nodes = min_nodes
-        self.max_nodes = max_nodes
-        self.start_hook = start_hook
-        self.stop_hook = stop_hook
-        self.deps = deps
-
-    def update_min_nodes(self, new_min):
-        if self.min_nodes:
-            self.min_nodes = max(self.min_nodes, new_min)
-        else:
-            self.min_nodes = new_min
+class _Role(namedtuple('_Role',
+                       'min_nodes max_nodes start_hook stop_hook deps')):
 
     def _println(self, indent, field):
         print '%s%s: %s' % (indent*'\t', field, str(getattr(self, field)))
@@ -30,24 +18,13 @@ class Role(object):
         self._println(indent, 'deps')
 
 
-class Dep(namedtuple('Dep', 'role ratio')):
+class _Dep(namedtuple('Dep', 'role ratio')):
 
     @staticmethod
     def convert_list(deps_list):
         if deps_list:
-            return [Dep(d[0], d[1]) for d in deps_list]
+            return [_Dep(d[0], d[1]) for d in deps_list]
         return []
-
-
-class Diff(object):
-
-    def __init__(self, min_nodes, max_nodes):
-        self.min_nodes = min_nodes
-        self.max_nodes = max_nodes
-
-    def increment(self):
-        self.min_nodes += 1
-        self.max_nodes += 1
 
 
 class Spec(object):
@@ -88,34 +65,30 @@ class Spec(object):
 
         for role_name in iter(spec):
             try:
-                role = Role(min_nodes=spec[role_name]['min_nodes'],
+                role = _Role(min_nodes=spec[role_name]['min_nodes'],
                             max_nodes=spec[role_name]['max_nodes'],
                             start_hook=spec[role_name]['start_hook'],
                             stop_hook=spec[role_name]['stop_hook'],
-                            deps=Dep.convert_list(spec[role_name]['deps']))
+                            deps=_Dep.convert_list(spec[role_name]['deps']))
                 self._roles[role_name] = role
             except KeyError, key:
                 raise ValueError('Node type \'%s\' missing required field %s' %
                                  (role_name, key))
 
-        # infer minimums
-        for depender_name in iter(self._roles):
-            depender = self._roles[depender_name]
-            deps = depender.deps
-            for dep in deps:
+        for name, depender in self._roles.iteritems():
+            for dep in depender.deps:
                 dependee = self._roles[dep.role]
-                new_min = int(ceil(depender.min_nodes / dep.ratio))
-                dependee.update_min_nodes(new_min)
+                new_min = int(ceil(float(depender.min_nodes) / dep.ratio))
+                if not dependee.min_nodes or new_min > dependee.min_nodes:
+                    self._roles[dep.role] = dependee._replace(min_nodes=new_min)
 
     def diff(self, state):
         diffs = {}
         for role in iter(self._roles):
-            diffs[role] = Diff(self._roles[role].min_nodes,
-                               self._roles[role].max_nodes)
+            diffs[role] = -self._roles[role].min_nodes
         for node in state:
             for role in state[node]:
-                print role, diffs[role].min_nodes, type(diffs[role].min_nodes)
-                diffs[role].increment()
+                diffs[role] += 1
         print diffs
 
     def dump(self):
@@ -131,6 +104,6 @@ if __name__ == '__main__':
 
 
     state = {u'localhost:2182': ['secondary_head'], u'localhost:2183': ['nc', 'hadoop', 'hadoop'], u'localhost:2181': ['primary_head'], u'localhost:2184': ['nc', 'hadoop', 'hadoop'], u'localhost:2185': ['nc', 'hadoop', 'hadoop']}
-    # spec.diff(state)
+    spec.diff(state)
 
     spec.dump()
