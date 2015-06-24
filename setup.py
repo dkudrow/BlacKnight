56bc373d93b4d76a04e647054cf3069933e20893
Daniel Kudrow <mail@danielkudrow.com>
2015-06-24 12:22:50 -0700

    start setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='blacknight',
+      version='0.1',
+      description='Fault-Management for Cloud Software Appliances',
+      Author='Daniel Kudrow',
+      author_email='dkudrow@cs.ucsb.edu',
+      url='https://github.com/dkudrow/BlacKnight/',
+      py_modules=['blacknight'],
+      )
