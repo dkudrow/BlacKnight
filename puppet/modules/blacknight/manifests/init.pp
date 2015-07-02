69d65dc25b0de957b877dd7cd2f0fb202be1c8ac
Daniel Kudrow <mail@danielkudrow.com>
2015-07-02 00:49:23 -0700

    use cloudera's zookeeper rpm

--- puppet/modules/blacknight/manifests/init.pp
+++ puppet/modules/blacknight/manifests/init.pp
@@ -1,9 +1,27 @@
 # modules/blacknight/manifests/init.pp
 
 class blacknight {
+
+  # Note: we will use cloudera's ZooKeeper rpm for simplicity
+  package { 'python-pip', 'cloudera-cdh4' :
+    ensure 'present'
+  }
+
   package { 'bkacknight' :
     provider => 'pip',
     ensure   => 'present',
     source   => '../../../../'
+    require  => Package[ 'python-pip' ]
   }
+
+  package { 'zookeeper-server':
+    ensure  => 'present',
+    require => Package[ 'cloudera-cdh4' ]
+  }
+
+  service {'zookeeper-server' :
+    ensure  => 'running',
+    require => Package[ 'zookeeper-server' ]
+  }
+
 }
